6088021eacc0029b883407ef4eabc6d09f8e20aa
Akash Singh <2200520319002@ietlucknow.ac.in>
2024-08-08 19:16:52 +0530

    Ak

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -44,7 +44,7 @@ function Catalog() {
       })()
     }
   }, [categoryId])
-
+  
   if (loading || !catalogPageData) {
     return (
       <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
